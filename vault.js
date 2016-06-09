d59c14c03fb211ce7623896d551406a2c7614a50
panicboy <asasaki@gmail.com>
2016-06-08 20:05:51 -1000

    Working

--- vault.js
+++ vault.js
@@ -1,4 +1,18 @@
 'use strict';
 module.exports = function() {
-  
+  var warehouse = {};
+  function _setValue(theKey,theValue) {
+    warehouse[theKey] = theValue;
+  }
+  function _getValue(theKey){
+    if(warehouse[theKey] === undefined){
+      return null;
+    } else {
+      return warehouse[theKey];
+    }
+  }
+  return {
+    setValue: _setValue,
+    getValue: _getValue
+  };
 };
